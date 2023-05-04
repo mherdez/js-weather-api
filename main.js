a88e0140f1c0131baeb7415e999665f8854c0ffc
Manuel Herdez <herdez@gmail.com>
2023-05-03 18:10:13 -0600

    html card structure

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-// import './style.css';
+import './style.css';
 import { card } from './src/app';
 
 const $app = document.querySelector('#app');
-$app.append(card())
+// $app.append(card())
+card()
